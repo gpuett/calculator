2aa6f300a99ed7c573c08b2dbd225206fba3cc4b
Garnett Puett and Kelsey Guerra <kelsey@>
2018-03-21 10:47:02 -0700

    add forms and functionality to other arithmetical calculations.

--- js/scripts.js
+++ js/scripts.js
@@ -18,15 +18,43 @@ var divide = function(number1, number2) {
 
 
 
-
-
-
 $(document).ready(function(){
   $("form#add").submit(function(event) {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#addoutput").text(result);
+  });
+});
+
+
+$(document).ready(function(){
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#suboutput").text(result);
+  });
+});
+
+$(document).ready(function(){
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var result = multiply(number1, number2);
+    $("#muloutput").text(result);
+  });
+});
+
+$(document).ready(function(){
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#divoutput").text(result);
   });
 });
