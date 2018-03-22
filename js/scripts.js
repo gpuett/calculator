d116bf512aaf9a2c833920799fbb70a6ef5b2551
Garnett Puett and Isaac Shoman <isaac@>
2018-03-22 14:55:31 -0700

    add branching element to calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -19,42 +19,21 @@ var divide = function(number1, number2) {
 
 
 $(document).ready(function(){
-  $("form#add").submit(function(event) {
+  $("#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#addoutput").text(result);
-  });
-});
-
-
-$(document).ready(function(){
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = subtract(number1, number2);
-    $("#suboutput").text(result);
-  });
-});
-
-$(document).ready(function(){
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#mul1").val());
-    var number2 = parseInt($("#mul2").val());
-    var result = multiply(number1, number2);
-    $("#muloutput").text(result);
-  });
-});
-
-$(document).ready(function(){
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#div1").val());
-    var number2 = parseInt($("#div2").val());
-    var result = divide(number1, number2);
-    $("#divoutput").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
 });
